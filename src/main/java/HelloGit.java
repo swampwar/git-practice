e3f3ed845b8134f74680d68497194fe3a0e170cf
swampwar <dkdfl88@gmail.com>
2020-04-05 11:24:41 +0900

    마스터2

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("하이");
         System.out.println("마스터에 커밋할꺼야");
+        System.out.println("마스터에 커밋할꺼야222");
     }
 }
