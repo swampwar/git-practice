077df0ec624ae04dadccebd343c653b51668b68d
swampwar <dkdfl88@gmail.com>
2020-04-05 11:53:41 +0900

    마스터 매소드 생성

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -2,4 +2,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("하이");
     }
+
+    public void master(){
+        System.out.println("마스터!");
+    }
 }
